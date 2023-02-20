1759b555f5ea1ca74afbbad0852ad7cff2840d83
dlimeng <mengyoupanshan@gmail.com>
2023-02-20 11:20:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const LIMIT_AI_IMAGE_COUNT = 5;
 const LIMIT_COUNT_RESPONSE = '对不起，因为ChatGPT调用收费，您的免费使用额度已用完~'
 
 const configuration = new Configuration({
-  apiKey: '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',
+  apiKey: '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',
 });
 
 const openai = new OpenAIApi(configuration);
